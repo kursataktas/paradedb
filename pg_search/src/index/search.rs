525f0d434be25c8e5c72ecfbb3551fb0df38f66b
Ming Ying <ming.ying.nyc@gmail.com>
2024-10-17 15:22:59 -0400

    fixed lock issues

--- pg_search/src/index/search.rs
+++ pg_search/src/index/search.rs
@@ -252,7 +252,7 @@ impl<'de> Deserialize<'de> for SearchIndex {
             .tantivy_dir_path(true)
             .expect("tantivy directory path should be valid");
 
-        let tantivy_dir = BlockingDirectory(directory.index_oid);
+        let tantivy_dir = BlockingDirectory::new(directory.index_oid);
         let mut underlying_index = Index::open(tantivy_dir).map_err(serde::de::Error::custom)?;
         // We need to setup tokenizers again after retrieving an index from disk.
         Self::setup_tokenizers(&mut underlying_index, &schema);
