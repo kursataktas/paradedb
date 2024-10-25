704ba72ebfa823f2e6b184ddb7040bd4b8c2e165
Ming Ying <ming.ying.nyc@gmail.com>
2024-10-25 16:40:39 -0400

    create channel crate

--- pg_search/src/index/mod.rs
+++ pg_search/src/index/mod.rs
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+pub mod channel;
 pub mod directory;
 pub mod reader;
 pub mod search;
