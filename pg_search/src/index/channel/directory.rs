b4c18e4d6a1cd5fac88588de255e4e1ab7294bd6
Ming Ying <ming.ying.nyc@gmail.com>
2024-10-25 17:06:22 -0400

    impl channeldirectory wip

--- pg_search/src/index/channel/directory.rs
+++ pg_search/src/index/channel/directory.rs
@@ -1,8 +1,13 @@
 use crossbeam::channel::{Receiver, Sender};
 use std::path::{Path, PathBuf};
+use std::sync::Arc;
 use std::{io, io::Cursor, ops::Range, result};
 use tantivy::directory::error::{DeleteError, LockError, OpenReadError, OpenWriteError};
+use tantivy::directory::{DirectoryLock, FileHandle, Lock, WatchCallback, WatchHandle, WritePtr};
+use tantivy::Directory;
 
+use super::reader::ChannelReader;
+use super::writer::ChannelWriter;
 use crate::postgres::storage::segment_handle::SegmentHandle;
 
 pub enum ChannelRequest {
@@ -21,11 +26,13 @@ pub enum ChannelResponse {
     AtomicWriteAck,
 }
 
-struct ChannelDirectory {
+#[derive(Clone, Debug)]
+pub struct ChannelDirectory {
     request_sender: Sender<ChannelRequest>,
     request_receiver: Receiver<ChannelResponse>,
     response_sender: Sender<ChannelRequest>,
     response_receiver: Receiver<ChannelResponse>,
+    relation_oid: u32
 }
 
 // A directory that actually forwards all read/write requests to a channel
@@ -34,6 +41,7 @@ impl ChannelDirectory {
     pub fn new(
         request_channel: (Sender<ChannelRequest>, Receiver<ChannelResponse>),
         response_channel: (Sender<ChannelRequest>, Receiver<ChannelResponse>),
+        relation_oid: u32
     ) -> Self {
         let (request_sender, request_receiver) = request_channel;
         let (response_sender, response_receiver) = response_channel;
@@ -43,7 +51,38 @@ impl ChannelDirectory {
             response_receiver,
             response_sender,
             request_receiver,
+            relation_oid
         }
+    }
+}
+
+impl Directory for ChannelDirectory {
+    fn get_file_handle(&self, path: &Path) -> Result<Arc<dyn FileHandle>, OpenReadError> {
+        Ok(Arc::new(unsafe {
+            ChannelReader::new(
+                self.relation_oid,
+                path,
+                self.request_sender.clone(),
+                self.request_receiver.clone(),
+            )
+            .map_err(|e| {
+                OpenReadError::wrap_io_error(
+                    io::Error::new(io::ErrorKind::Other, format!("{:?}", e)),
+                    path.to_path_buf(),
+                )
+            })?
+        }))
+    }
+
+    fn open_write(&self, path: &Path) -> result::Result<WritePtr, OpenWriteError> {
+        Ok(io::BufWriter::new(Box::new(unsafe {
+            ChannelWriter::new(
+                self.relation_oid,
+                path,
+                self.request_sender.clone(),
+                self.request_receiver.clone(),
+            )
+        })))
     }
 
     fn atomic_read(&self, path: &Path) -> result::Result<Vec<u8>, OpenReadError> {
@@ -63,7 +102,7 @@ impl ChannelDirectory {
         }
     }
 
-    fn atomic_write(&self, path: &Path, data: &[u8]) -> result::Result<(), OpenWriteError> {
+    fn atomic_write(&self, path: &Path, data: &[u8]) -> io::Result<()> {
         self.request_sender
             .send(ChannelRequest::AtomicWrite(
                 path.to_path_buf(),
@@ -73,13 +112,36 @@ impl ChannelDirectory {
 
         match self.response_receiver.recv().unwrap() {
             ChannelResponse::AtomicWriteAck => Ok(()),
-            unexpected => Err(OpenWriteError::wrap_io_error(
-                io::Error::new(
-                    io::ErrorKind::Other,
-                    format!("atomic_write unexpected response {:?}", unexpected),
-                ),
-                path.to_path_buf(),
+            unexpected => Err(io::Error::new(
+                io::ErrorKind::Other,
+                format!("atomic_write unexpected response {:?}", unexpected),
             )),
         }
+    }
+
+    fn delete(&self, path: &Path) -> result::Result<(), DeleteError> {
+        todo!("directory delete");
+    }
+
+    fn exists(&self, path: &Path) -> Result<bool, OpenReadError> {
+        todo!("directory exists");
+    }
+
+    fn acquire_lock(&self, lock: &Lock) -> result::Result<DirectoryLock, LockError> {
+        // The lock itself doesn't seem to actually be used anywhere by Tantivy
+        // acquire_lock seems to be a place for us to implement our own locking behavior
+        // which we don't need since pg_sys::ReadBuffer is already handling this
+        Ok(DirectoryLock::from(Box::new(Lock {
+            filepath: lock.filepath.clone(),
+            is_blocking: true,
+        })))
+    }
+
+    fn watch(&self, watch_callback: WatchCallback) -> tantivy::Result<WatchHandle> {
+        todo!("directory watch");
+    }
+
+    fn sync_directory(&self) -> io::Result<()> {
+        Ok(())
     }
 }
