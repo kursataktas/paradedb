ed033a7951672630f60d81e848ce6e6f110cd851
Ming Ying <ming.ying.nyc@gmail.com>
2024-10-25 18:17:01 -0400

    thread approach showing promise

--- pg_search/src/postgres/delete.rs
+++ pg_search/src/postgres/delete.rs
@@ -17,11 +17,13 @@
 
 use crate::index::WriterResources;
 use crate::index::channel::directory::{ChannelDirectory, ChannelRequest, ChannelResponse};
+use crate::index::SearchIndex;
 use crate::postgres::index::open_search_index;
 use crate::postgres::options::SearchIndexCreateOptions;
 use pgrx::{pg_sys::ItemPointerData, *};
 use tantivy::index::Index;
 use tantivy::indexer::IndexWriter;
+use tantivy::IndexReader;
 
 #[pg_guard]
 pub extern "C" fn ambulkdelete(
@@ -33,32 +35,27 @@ pub extern "C" fn ambulkdelete(
     let info = unsafe { PgBox::from_pg(info) };
     let mut stats = unsafe { PgBox::from_pg(stats) };
     let index_relation = unsafe { PgRelation::from_pg(info.index) };
-
+    let index_oid: u32 = index_relation.oid().into();
+    let search_index =
+        open_search_index(&index_relation).expect("should be able to open search index");
     let request_channel = crossbeam::channel::unbounded::<ChannelRequest>();
     let response_channel = crossbeam::channel::unbounded::<ChannelResponse>();
 
-    let channel_directory = ChannelDirectory::new(
-        request_channel,
-        response_channel,
-        index_relation.oid().into(),
-    );
-    let index = Index::open(channel_directory).expect("index should be openable");
-    // TODO: Make this configurable
-    let writer: IndexWriter = index
-        .writer(500_000_000)
-        .expect("writer should be openable");
-
-    let search_index =
-        open_search_index(&index_relation).expect("should be able to open search index");
-    let reader = search_index
-        .get_reader()
-        .unwrap_or_else(|err| panic!("error loading index reader in bulkdelete: {err}"));
-    let options = index_relation.rd_options as *mut SearchIndexCreateOptions;
-    let mut writer = search_index
-        .get_writer(WriterResources::Vacuum, unsafe {
-            options.as_ref().unwrap()
-        })
-        .unwrap_or_else(|err| panic!("error loading index writer in bulkdelete: {err}"));
+    std::thread::spawn(move || {
+        let channel_directory = ChannelDirectory::new(request_channel, response_channel, index_oid);
+        let underlying_index = Index::open(channel_directory).expect("channel index should open");
+        let channel_index = SearchIndex {
+            underlying_index,
+            directory: search_index.directory,
+            schema: search_index.schema,
+        };
+        let reader = channel_index
+            .get_reader()
+            .unwrap_or_else(|err| panic!("error loading index reader in bulkdelete: {err}"));
+        let writer = channel_index
+            .get_writer()
+            .unwrap_or_else(|err| panic!("error loading index writer in bulkdelete: {err}"));
+    });
 
     if stats.is_null() {
         stats = unsafe {
@@ -68,26 +65,28 @@ pub extern "C" fn ambulkdelete(
         };
     }
 
-    if let Some(actual_callback) = callback {
-        let should_delete = |ctid_val| unsafe {
-            let mut ctid = ItemPointerData::default();
-            crate::postgres::utils::u64_to_item_pointer(ctid_val, &mut ctid);
-            actual_callback(&mut ctid, callback_state)
-        };
-        match search_index.delete(&reader, &writer, should_delete) {
-            Ok((deleted, not_deleted)) => {
-                stats.pages_deleted += deleted;
-                stats.num_pages += not_deleted;
-            }
-            Err(err) => {
-                panic!("error: {err:?}")
-            }
-        }
-    }
+    pgrx::info!("got here");
 
-    writer
-        .commit()
-        .unwrap_or_else(|err| panic!("error committing to index in ambulkdelete: {err}"));
+    // if let Some(actual_callback) = callback {
+    //     let should_delete = |ctid_val| unsafe {
+    //         let mut ctid = ItemPointerData::default();
+    //         crate::postgres::utils::u64_to_item_pointer(ctid_val, &mut ctid);
+    //         actual_callback(&mut ctid, callback_state)
+    //     };
+    //     match channel_index.delete(&reader, &writer, should_delete) {
+    //         Ok((deleted, not_deleted)) => {
+    //             stats.pages_deleted += deleted;
+    //             stats.num_pages += not_deleted;
+    //         }
+    //         Err(err) => {
+    //             panic!("error: {err:?}")
+    //         }
+    //     }
+    // }
+
+    // writer
+    //     .commit()
+    //     .unwrap_or_else(|err| panic!("error committing to index in ambulkdelete: {err}"));
 
     stats.into_pg()
 }
